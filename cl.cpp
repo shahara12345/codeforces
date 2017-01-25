a2bf52648b9ad5215106c588cd44eef482c10eae
ak9969 <jakshat100@gmail.com>
2017-01-26 00:07:47 +0530

    dfs

---
+++ cl.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+string a; int h=0,k=0;
+cin>>a;
+if (a[0]>=97)
+    {
+for (int i=1;i<a.size();i++)
+    {
+        if (a[i]>=65 && a[i]<=90)
+            {
+                h++;
+            }
+    }
+    if (h==a.size()-1)
+        {
+            a[0]=a[0]-32;
+            for (int i=1;i<a.size();i++)
+                {
+                    a[i]=a[i]+32;
+                }
+        }
+    }
+else
+    {
+        for (int i=0;i<a.size();i++)
+            {
+                if (a[i]>=65 && a[i]<=90)
+                    {
+                        k++;
+                    }
+            }
+        if (k==a.size())
+            {
+                for (int i=0;i<a.size();i++)
+                    {
+                        a[i]=a[i]+32;
+                    }
+            }
+    }
+cout<<a<<endl;
+}
