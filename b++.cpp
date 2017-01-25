18507674b94f0a166722b8204354c5a253e93eb7
ak9969 <jakshat100@gmail.com>
2017-01-26 00:12:48 +0530

    hkHJHgg

---
+++ b++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,x=0;
+	cin>>n;
+	while(n--)
+	{
+		char str[30];
+		cin>>str;
+		if(str[0]=='+' || str[1]=='+')
+            x++;
+        else if(str[0]=='-' || str[1]=='-')
+            x--;	
+	}
+	cout<<x;
+	return 0;
+}
