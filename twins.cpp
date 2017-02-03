f67a79751e93ca6ba22f41acef6cbcd0f4dcb721
ak9969 <jakshat100@gmail.com>
2017-02-03 18:54:16 +0530

    SAAAAS

---
+++ twins.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math>
+using namespace std;
+int main()
+{
+	int k[50];
+	int n,max,c=0;
+	cin>>n;
+	if(n==0)
+	{
+		cout<<"0";
+	}
+	vector<int> v;
+	sort(v.begin(),v.end());
+	
+
+	cout<<v
+}
